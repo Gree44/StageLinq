00f4503e3f8294a1ccf1bd8c766da9e2982a8594
Gree44 <jaja777111@gmail.com>
2026-02-09 14:03:20 +0100

    no stub

--- types/metadata-connect.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'metadata-connect' {
-  export interface ExtractedMetadata {
-    [key: string]: unknown;
-  }
-
-  export function extractMetadata(input: unknown): Promise<ExtractedMetadata>;
-  export function isExtensionSupported(ext: string): boolean;
-}
